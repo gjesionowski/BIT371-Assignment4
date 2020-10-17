16ca345b6980e345bdabdc9b4912bba981b68105
gjesionowski <gregory.a.jesionowski@gmail.com>
2020-10-16 17:33:14 -0700

    Shows index of selected list item

--- app/src/main/java/com/example/application4/MainActivity.java
+++ app/src/main/java/com/example/application4/MainActivity.java
@@ -6,24 +6,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String[] cities = {"Seattle", "Bothell", "Kirkland", "Bellevue", "Lynnwood", "Renton", "Redmond", "Spokane", "Vancouver", "Tacoma", "Olympia", "Bellingham", "Arlington", "Everett", "Woodinville", "Monroe", "New Castle", "Ballard", "Lacey"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.main_activity);
 
-    public void login(View view){
-    Log.i("INFO", "Attempting login.");
-    Intent intent = new Intent(getApplicationContext(), UserConfirmationActivity.class);
-        EditText username = findViewById(R.id.editTextTextPersonName);
-        intent.putExtra("username", username.getText().toString());
-        EditText password = findViewById(R.id.editTextTextPassword);
-        intent.putExtra("password", password.getText().toString());
-    startActivity(intent);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.listview_layout, R.id.city, cities);
+
+        ListView lv = findViewById(R.id.city_list);
+        lv.setAdapter(adapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.i("INFO", "The index is: " + i);
+            }
+        });
     }
 }
